3a9eeda0b507c4166420fdde5abb524150b0adfa
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-22 23:32:09 +0200

    Add input to lists

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,11 @@ let $myList = document.querySelector('.myList');
 
 function addSubject() {
     let li = document.createElement('li');
+    let input = document.createElement('input');
     $myList.appendChild(li);
+    input.type = 'Number';
+    li.appendChild(input);
+    console.log(typeof input)
 }
 
 $addSubject.addEventListener('click', addSubject);

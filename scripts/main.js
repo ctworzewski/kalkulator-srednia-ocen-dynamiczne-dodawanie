e10fe4a1e708ce0d6997afd7bcf692fc36914220
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-22 23:40:53 +0200

    Add dynamic text to li with input

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,11 @@ let $addSubject = document.querySelector('.addSubject');
 let $myList = document.querySelector('.myList');
 
 function addSubject() {
+    let $nameSubject = document.querySelector('.nameSubject').value;
     let li = document.createElement('li');
     let input = document.createElement('input');
+    li.textContent = $nameSubject;
+    f
     $myList.appendChild(li);
     input.type = 'Number';
     li.appendChild(input);
